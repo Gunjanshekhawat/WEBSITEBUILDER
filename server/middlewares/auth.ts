0247328c0205992d2dff267bb8894fd9a1f5f733
Gunjan <shekhawat.gunjan2003@gmail.com>
2026-04-26 15:32:52 +0530

    cookie4

--- server/middlewares/auth.ts
+++ server/middlewares/auth.ts
@@ -7,6 +7,7 @@ export const protect = async (req: Request, res: Response, next: NextFunction) =
         const session = await auth.api.getSession({
             headers: fromNodeHeaders(req.headers)
         })
+        console.log("headers:", req.headers);
        
 
         if (!session || !session?.user) {
