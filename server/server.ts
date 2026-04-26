e7e3de57a34e115af8ab6a8bcda8d840145a8f99
Gunjan <shekhawat.gunjan2003@gmail.com>
2026-04-26 19:38:37 +0530

    view11

--- server/server.ts
+++ server/server.ts
@@ -57,8 +57,7 @@ const __dirname = path.dirname(__filename);
 app.use(express.static(path.join(__dirname, "../client/dist")));
 
 // SPA fallback
-app.get("/*", (req, res) => {
+app.use((req, res) => {
   res.sendFile(path.join(__dirname, "../client/dist/index.html"));
 });
-
 
