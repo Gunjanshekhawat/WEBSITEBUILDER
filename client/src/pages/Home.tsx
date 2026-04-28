ab1fe02c3ebe239fc427901d2893aa67975f4670
Gunjan <shekhawat.gunjan2003@gmail.com>
2026-04-28 11:07:21 +0530

    responasiveness9

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -40,10 +40,10 @@ function Home() {
 
 
   return (
- <section className="flex flex-col items-center text-white text-sm   px-4 sm:px-6 lg:px-8 mt-16 sm:mt-18 lg:mt-22 max-w-7xl mx-auto font-poppins">
+ <section className="flex flex-col items-center text-white text-sm  px-4 sm:px-8 lg:px-8 mt-16 sm:mt-20 lg:mt-22 max-w-7xl mx-auto font-poppins">
          
 
-        <h1 className="text-center text-2xl sm:text-6xl md:text-5xl lg:text-6xl leading-tight md:leading-[70px] mt-4 sm:mt-8 font-semibold max-w-3xl mx-auto">
+        <h1 className="text-center text-4xl sm:text-6xl md:text-5xl lg:text-6xl leading-tight md:leading-[70px] mt-4 sm:mt-8 font-semibold max-w-3xl mx-auto">
           Turn thoughts into websites instantly, with AI.
         </h1>
 
