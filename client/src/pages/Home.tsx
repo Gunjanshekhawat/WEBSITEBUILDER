626d2d7b2768552b6b568112cf94c6a8f4515d9f
Gunjan <shekhawat.gunjan2003@gmail.com>
2026-04-28 09:42:15 +0530

    responasiveness

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -40,19 +40,19 @@ function Home() {
 
 
   return (
- <section className="flex flex-col items-center text-white text-sm pb-20 px-25 mt-25 font-poppins">
+ <section className="flex flex-col items-center text-white text-sm pb-12 sm:pb-16 lg:pb-20 px-4 sm:px-6 lg:px-10 mt-8 sm:mt-12 lg:mt-24 max-w-7xl mx-auto font-poppins">
          
 
-        <h1 className="text-center text-[45px] leading-[48px] md:text-6xl md:leading-[70px] mt-4 font-semibold max-w-3xl">
+        <h1 className="text-center text-3xl sm:text-4xl md:text-5xl lg:text-6xl leading-tight md:leading-[70px] mt-4 sm:mt-8 font-semibold max-w-3xl mx-auto">
           Turn thoughts into websites instantly, with AI.
         </h1>
 
-        <p className="text-center text-base max-w-md mt-2">
+        <p className="text-center text-sm sm:text-base max-w-md mx-auto mt-4 sm:mt-6">
           Create, customize and publish website faster than ever with our AI site.
         </p>
 
         <form onSubmit={onSubmitHandler} className="bg-white/10 max-w-4xl  h-30px w-full rounded-xl p-4 mt-10 border border-indigo-600/70 focus-within:ring-2 ring-indigo-500 transition-all">
-          <textarea onChange={(e) => setInput(e.target.value)} className="bg-transparent outline-none text-gray-300 resize-none w-full" rows={4} placeholder="Describe your presentation in details" required />
+          <textarea onChange={(e) => setInput(e.target.value)} className="bg-transparent outline-none text-gray-300 resize-none w-full flex-1 mb-4 sm:mb-0" rows={4} placeholder="Describe your presentation in details" required />
           <button className="ml-auto flex items-center gap-2 bg-gradient-to-r from-[#d34ddd] to-indigo-700 rounded-md px-4 py-2">
             {!loading ? 'Create with AI' :
             (
